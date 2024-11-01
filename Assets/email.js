ace2c3804a6449ddfc21a3b7a57a80b445c2496b
AleRomito <151970083+AleRomito@users.noreply.github.com>
2024-11-01 12:32:43 -0300

    Add files via upload

---
+++ Assets/email.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('button');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  btn.value = 'Enviando...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_t12745b';
+
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar';
+      alert('Enviado!');
+      resetForm(); // Llama a la función para limpiar el formulario
+    }, (err) => {
+      btn.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
+
+function resetForm() {
+  form.reset(); // Restablece los valores del formulario
+}

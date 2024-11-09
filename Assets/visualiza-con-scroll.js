f0a3ddcbbdb41b2986fe458fcf801c76600ffead
AleRomito <151970083+AleRomito@users.noreply.github.com>
2024-11-09 12:10:18 -0300

    Add files via upload

--- Assets/visualiza-con-scroll.js
+++ Assets/visualiza-con-scroll.js
@@ -1,28 +1,33 @@
 console.log("El archivo visualiza-con-scroll.js ha sido cargado.");
 
 document.addEventListener("DOMContentLoaded", function() {
-    const contenedores = document.querySelectorAll('.contenedor-info');
+    // Verificar si el ancho de la pantalla es adecuado para dispositivos móviles
+    if (window.innerWidth <= 768) {
+        const contenedores = document.querySelectorAll('.contenedor-info');
 
-    // Función para verificar la posición de los divs al hacer scroll
-    function checkDivPositions() {
-        const viewportHeight = window.innerHeight;
-        const viewportCenter = viewportHeight / 2;
+        // Función para verificar la posición de los divs al hacer scroll
+        function checkDivPositions() {
+            const viewportHeight = window.innerHeight;
+            const viewportCenter = viewportHeight / 2;
 
-        contenedores.forEach(contenedor => {
-            const rect = contenedor.getBoundingClientRect();
-            const divCenter = rect.top + (rect.height / 2);
+            contenedores.forEach(contenedor => {
+                const rect = contenedor.getBoundingClientRect();
+                const divCenter = rect.top + (rect.height / 2);
 
-            // Verificar si el centro del div está cerca del centro del viewport
-            if (divCenter >= viewportCenter - 50 && divCenter <= viewportCenter + 50) {
-                contenedor.classList.add('visible'); // Añadir clase visible
-            } else {
-                contenedor.classList.remove('visible'); // Quitar clase visible
-            }
-        });
+                // Verificar si el centro del div está cerca del centro del viewport
+                if (divCenter >= viewportCenter - 50 && divCenter <= viewportCenter + 50) {
+                    contenedor.classList.add('visible'); // Añadir clase visible
+                } else {
+                    contenedor.classList.remove('visible'); // Quitar clase visible
+                }
+            });
+        }
+
+        // Escuchar el evento de scroll
+        window.addEventListener('scroll', checkDivPositions);
+        // Llamar la función al cargar la página para asegurar el estado inicial
+        checkDivPositions();
+    } else {
+        console.log("El script no se ejecuta en pantallas grandes.");
     }
-
-    // Escuchar el evento de scroll
-    window.addEventListener('scroll', checkDivPositions);
-    // Llamar la función al cargar la página para asegurar el estado inicial
-    checkDivPositions();
 });
